d3c8368426c80e36d0dd30bffb49f79b7601425c
PCE <PCE@DESKTOP-GJBQRB1>
2025-07-03 16:43:54 +0500

    haram ap karlo

--- Dashboard.Designer.cs
+++ Dashboard.Designer.cs
@@ -28,7 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.guna2Panel2 = new Guna.UI2.WinForms.Guna2Panel();
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dashboard));
+            this.label2 = new System.Windows.Forms.Label();
+            this.profilepic = new Guna.UI2.WinForms.Guna2CirclePictureBox();
             this.labellogout = new System.Windows.Forms.LinkLabel();
             this.btnmanagebook = new Guna.UI2.WinForms.Guna2Button();
             this.btnliboperation = new Guna.UI2.WinForms.Guna2Button();
@@ -39,30 +42,45 @@
             this.btnclose = new Guna.UI2.WinForms.Guna2Button();
             this.label1 = new System.Windows.Forms.Label();
             this.panelmain = new Guna.UI2.WinForms.Guna2CustomGradientPanel();
-            this.profilepic = new Guna.UI2.WinForms.Guna2CirclePictureBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.guna2Panel2.SuspendLayout();
-            this.guna2Panel1.SuspendLayout();
+            this.btnmenu = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.slidebartimer = new System.Windows.Forms.Timer(this.components);
+            this.guna2Panel4 = new Guna.UI2.WinForms.Guna2Panel();
+            this.sidebar = new System.Windows.Forms.FlowLayoutPanel();
+            this.guna2Panel2 = new Guna.UI2.WinForms.Guna2Panel();
             ((System.ComponentModel.ISupportInitialize)(this.profilepic)).BeginInit();
+            this.guna2Panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnmenu)).BeginInit();
+            this.guna2Panel4.SuspendLayout();
+            this.sidebar.SuspendLayout();
+            this.guna2Panel2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // guna2Panel2
+            // label2
             // 
-            this.guna2Panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(110)))), ((int)(((byte)(75)))));
-            this.guna2Panel2.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(110)))), ((int)(((byte)(75)))));
-            this.guna2Panel2.Controls.Add(this.label2);
-            this.guna2Panel2.Controls.Add(this.profilepic);
-            this.guna2Panel2.Controls.Add(this.labellogout);
-            this.guna2Panel2.Controls.Add(this.btnmanagebook);
-            this.guna2Panel2.Controls.Add(this.btnliboperation);
-            this.guna2Panel2.Controls.Add(this.btnmemmanagement);
-            this.guna2Panel2.Controls.Add(this.btndashboard);
-            this.guna2Panel2.Dock = System.Windows.Forms.DockStyle.Left;
-            this.guna2Panel2.Location = new System.Drawing.Point(0, 0);
-            this.guna2Panel2.Name = "guna2Panel2";
-            this.guna2Panel2.Size = new System.Drawing.Size(252, 700);
-            this.guna2Panel2.TabIndex = 2;
-            this.guna2Panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.guna2Panel2_Paint);
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe Script", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(50, 215);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(155, 40);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "User Name";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // profilepic
+            // 
+            this.profilepic.BackColor = System.Drawing.Color.Transparent;
+            this.profilepic.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.profilepic.FillColor = System.Drawing.Color.DimGray;
+            this.profilepic.ImageRotate = 0F;
+            this.profilepic.Location = new System.Drawing.Point(44, 35);
+            this.profilepic.Name = "profilepic";
+            this.profilepic.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.profilepic.Size = new System.Drawing.Size(176, 177);
+            this.profilepic.TabIndex = 30;
+            this.profilepic.TabStop = false;
+            this.profilepic.Click += new System.EventHandler(this.profilepic_Click);
             // 
             // labellogout
             // 
@@ -70,12 +88,13 @@
             this.labellogout.AutoSize = true;
             this.labellogout.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labellogout.LinkColor = System.Drawing.Color.White;
-            this.labellogout.Location = new System.Drawing.Point(86, 650);
+            this.labellogout.Location = new System.Drawing.Point(83, 556);
             this.labellogout.Name = "labellogout";
             this.labellogout.Size = new System.Drawing.Size(109, 37);
             this.labellogout.TabIndex = 4;
             this.labellogout.TabStop = true;
             this.labellogout.Text = "Logout";
+            this.labellogout.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.labellogout_LinkClicked);
             // 
             // btnmanagebook
             // 
@@ -87,11 +106,12 @@
             this.btnmanagebook.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(110)))), ((int)(((byte)(75)))));
             this.btnmanagebook.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnmanagebook.ForeColor = System.Drawing.Color.White;
-            this.btnmanagebook.Location = new System.Drawing.Point(8, 484);
+            this.btnmanagebook.Location = new System.Drawing.Point(5, 473);
             this.btnmanagebook.Name = "btnmanagebook";
             this.btnmanagebook.Size = new System.Drawing.Size(241, 52);
             this.btnmanagebook.TabIndex = 3;
             this.btnmanagebook.Text = "Manage Books";
+            this.btnmanagebook.Click += new System.EventHandler(this.btnmanagebook_Click);
             // 
             // btnliboperation
             // 
@@ -103,11 +123,12 @@
             this.btnliboperation.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(110)))), ((int)(((byte)(75)))));
             this.btnliboperation.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnliboperation.ForeColor = System.Drawing.Color.White;
-            this.btnliboperation.Location = new System.Drawing.Point(8, 364);
+            this.btnliboperation.Location = new System.Drawing.Point(5, 353);
             this.btnliboperation.Name = "btnliboperation";
             this.btnliboperation.Size = new System.Drawing.Size(241, 52);
             this.btnliboperation.TabIndex = 2;
             this.btnliboperation.Text = "History";
+            this.btnliboperation.Click += new System.EventHandler(this.btnliboperation_Click);
             // 
             // btnmemmanagement
             // 
@@ -119,11 +140,12 @@
             this.btnmemmanagement.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(110)))), ((int)(((byte)(75)))));
             this.btnmemmanagement.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnmemmanagement.ForeColor = System.Drawing.Color.White;
-            this.btnmemmanagement.Location = new System.Drawing.Point(8, 424);
+            this.btnmemmanagement.Location = new System.Drawing.Point(5, 413);
             this.btnmemmanagement.Name = "btnmemmanagement";
             this.btnmemmanagement.Size = new System.Drawing.Size(241, 52);
             this.btnmemmanagement.TabIndex = 1;
             this.btnmemmanagement.Text = "Member Management";
+            this.btnmemmanagement.Click += new System.EventHandler(this.btnmemmanagement_Click);
             // 
             // btndashboard
             // 
@@ -135,11 +157,12 @@
             this.btndashboard.FillColor = System.Drawing.Color.White;
             this.btndashboard.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btndashboard.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(110)))), ((int)(((byte)(75)))));
-            this.btndashboard.Location = new System.Drawing.Point(8, 304);
+            this.btndashboard.Location = new System.Drawing.Point(5, 293);
             this.btndashboard.Name = "btndashboard";
             this.btndashboard.Size = new System.Drawing.Size(241, 52);
             this.btndashboard.TabIndex = 0;
             this.btndashboard.Text = "Dashboard";
+            this.btndashboard.Click += new System.EventHandler(this.btndashboard_Click);
             // 
             // guna2Panel3
             // 
@@ -148,6 +171,7 @@
             this.guna2Panel3.Name = "guna2Panel3";
             this.guna2Panel3.Size = new System.Drawing.Size(4, 700);
             this.guna2Panel3.TabIndex = 3;
+            this.guna2Panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.guna2Panel3_Paint);
             // 
             // guna2Panel1
             // 
@@ -160,6 +184,7 @@
             this.guna2Panel1.Name = "guna2Panel1";
             this.guna2Panel1.Size = new System.Drawing.Size(1044, 60);
             this.guna2Panel1.TabIndex = 4;
+            this.guna2Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.guna2Panel1_Paint);
             // 
             // btnclose
             // 
@@ -175,6 +200,7 @@
             this.btnclose.Size = new System.Drawing.Size(62, 51);
             this.btnclose.TabIndex = 1;
             this.btnclose.Text = "X";
+            this.btnclose.Click += new System.EventHandler(this.btnclose_Click);
             // 
             // label1
             // 
@@ -186,6 +212,7 @@
             this.label1.Size = new System.Drawing.Size(373, 47);
             this.label1.TabIndex = 0;
             this.label1.Text = "MZ LIBRARY SYSTEM";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // panelmain
             // 
@@ -195,30 +222,75 @@
             this.panelmain.Name = "panelmain";
             this.panelmain.Size = new System.Drawing.Size(1044, 640);
             this.panelmain.TabIndex = 5;
+            this.panelmain.Paint += new System.Windows.Forms.PaintEventHandler(this.panelmain_Paint);
             // 
-            // profilepic
+            // btnmenu
             // 
-            this.profilepic.BackColor = System.Drawing.Color.Transparent;
-            this.profilepic.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.profilepic.FillColor = System.Drawing.Color.DimGray;
-            this.profilepic.ImageRotate = 0F;
-            this.profilepic.Location = new System.Drawing.Point(40, 14);
-            this.profilepic.Name = "profilepic";
-            this.profilepic.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
-            this.profilepic.Size = new System.Drawing.Size(176, 177);
-            this.profilepic.TabIndex = 30;
-            this.profilepic.TabStop = false;
+            this.btnmenu.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnmenu.Image = ((System.Drawing.Image)(resources.GetObject("btnmenu.Image")));
+            this.btnmenu.Location = new System.Drawing.Point(20, 20);
+            this.btnmenu.Name = "btnmenu";
+            this.btnmenu.Size = new System.Drawing.Size(30, 30);
+            this.btnmenu.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btnmenu.TabIndex = 31;
+            this.btnmenu.TabStop = false;
+            this.btnmenu.Click += new System.EventHandler(this.btnmenu_Click);
             // 
-            // label2
+            // label3
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe Script", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(51, 194);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(155, 40);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "User Name";
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Black", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(92, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 37);
+            this.label3.TabIndex = 32;
+            this.label3.Text = "Menu";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // slidebartimer
+            // 
+            this.slidebartimer.Interval = 1;
+            this.slidebartimer.Tick += new System.EventHandler(this.slidebartime_Tick);
+            // 
+            // guna2Panel4
+            // 
+            this.guna2Panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(110)))), ((int)(((byte)(75)))));
+            this.guna2Panel4.Controls.Add(this.label3);
+            this.guna2Panel4.Controls.Add(this.btnmenu);
+            this.guna2Panel4.Location = new System.Drawing.Point(3, 3);
+            this.guna2Panel4.Name = "guna2Panel4";
+            this.guna2Panel4.Size = new System.Drawing.Size(248, 70);
+            this.guna2Panel4.TabIndex = 1;
+            // 
+            // sidebar
+            // 
+            this.sidebar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(110)))), ((int)(((byte)(75)))));
+            this.sidebar.Controls.Add(this.guna2Panel4);
+            this.sidebar.Controls.Add(this.guna2Panel2);
+            this.sidebar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.sidebar.Location = new System.Drawing.Point(0, 0);
+            this.sidebar.MaximumSize = new System.Drawing.Size(252, 700);
+            this.sidebar.MinimumSize = new System.Drawing.Size(252, 700);
+            this.sidebar.Name = "sidebar";
+            this.sidebar.Size = new System.Drawing.Size(252, 700);
+            this.sidebar.TabIndex = 2;
+            // 
+            // guna2Panel2
+            // 
+            this.guna2Panel2.Controls.Add(this.label2);
+            this.guna2Panel2.Controls.Add(this.btnliboperation);
+            this.guna2Panel2.Controls.Add(this.profilepic);
+            this.guna2Panel2.Controls.Add(this.btndashboard);
+            this.guna2Panel2.Controls.Add(this.btnmanagebook);
+            this.guna2Panel2.Controls.Add(this.labellogout);
+            this.guna2Panel2.Controls.Add(this.btnmemmanagement);
+            this.guna2Panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.guna2Panel2.Location = new System.Drawing.Point(3, 79);
+            this.guna2Panel2.Name = "guna2Panel2";
+            this.guna2Panel2.Size = new System.Drawing.Size(248, 609);
+            this.guna2Panel2.TabIndex = 2;
+            this.guna2Panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.guna2Panel2_Paint_2);
             // 
             // Dashboard
             // 
@@ -226,27 +298,29 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1300, 700);
-            this.Controls.Add(this.panelmain);
             this.Controls.Add(this.guna2Panel1);
             this.Controls.Add(this.guna2Panel3);
-            this.Controls.Add(this.guna2Panel2);
+            this.Controls.Add(this.sidebar);
+            this.Controls.Add(this.panelmain);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Dashboard";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Dashboard";
             this.Load += new System.EventHandler(this.Dashboard_Load);
-            this.guna2Panel2.ResumeLayout(false);
-            this.guna2Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.profilepic)).EndInit();
             this.guna2Panel1.ResumeLayout(false);
             this.guna2Panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.profilepic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnmenu)).EndInit();
+            this.guna2Panel4.ResumeLayout(false);
+            this.guna2Panel4.PerformLayout();
+            this.sidebar.ResumeLayout(false);
+            this.guna2Panel2.ResumeLayout(false);
+            this.guna2Panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private Guna.UI2.WinForms.Guna2Panel guna2Panel2;
         private System.Windows.Forms.LinkLabel labellogout;
         private Guna.UI2.WinForms.Guna2Button btnmanagebook;
         private Guna.UI2.WinForms.Guna2Button btnliboperation;
@@ -259,5 +333,11 @@
         private Guna.UI2.WinForms.Guna2CustomGradientPanel panelmain;
         private Guna.UI2.WinForms.Guna2CirclePictureBox profilepic;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox btnmenu;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer slidebartimer;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel4;
+        private System.Windows.Forms.FlowLayoutPanel sidebar;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel2;
     }
 }
--- Dashboard.cs
+++ Dashboard.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace bus_management_system
-{
+{ 
     public partial class Dashboard : Form
     {
         public Dashboard()
@@ -17,12 +17,117 @@ namespace bus_management_system
             InitializeComponent();
         }
 
+        bool slidebarexpand;
         private void Dashboard_Load(object sender, EventArgs e)
         {
 
         }
 
         private void guna2Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void slidebartime_Tick(object sender, EventArgs e)
+        {
+            if (slidebarexpand)
+            {
+                slidebar.Width -= 58;
+                if (slidebar.Width == slidebar.MinimumSize.Width)
+                {
+                    slidebarexpand = false;
+                    slidebartimer.Stop();
+                }
+            }
+            else
+            {
+                slidebar.Width += 164;
+                if (slidebar.Width == slidebar.MaximumSize.Width)
+                {
+                    slidebarexpand = true;
+                    slidebartimer.Stop();
+                }
+
+
+            }
+        }
+
+        private void panelmain_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void profilepic_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labellogout_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void btnmanagebook_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnliboperation_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnmemmanagement_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btndashboard_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2Panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void guna2Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnclose_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2Panel2_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnmenu_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2Panel2_Paint_2(object sender, PaintEventArgs e)
         {
 
         }
